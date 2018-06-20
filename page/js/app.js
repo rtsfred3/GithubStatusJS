8ece23a50d4e760c93c332ef99ef735377706b73
Ryan <rtsfred3@gmail.com>
2018-06-20 16:42:22 -0700

    Updates to /page/

--- page/js/app.js
+++ page/js/app.js
@@ -10,13 +10,19 @@ function Messages(mess){
     var out = '<i>No new messages</i>';
     return;
   }else{
-    var out = '<table width="100%">';
+    var out = '';
     for(var i = 0; i < mess.length; i++){
+      var a = '';
+      if(i != mess.length-1){ a += ' text-margin-bottom'; }
+
+      out += '<div class="status-box ' + mess[i].status.toLowerCase() + '"><span class="message-status"><div style="float:right;">' + mess[i].status.toLowerCase() + '</div></span></div>';
+
       var date = new Date(mess[i].created_on).toString();
-      date = '<span class="date">' + date.substring(4,15) +  ' at ' + date.substring(16,24) + '</span>';
-      out += '<tr><td class="' + mess[i].status.toLowerCase() + 't size"></td><td><span>' + mess[i].body + '</span><br />'+date+'</tr>';
+      date = '<span class="date empty">' + date.substring(4,15) +  ' at ' + date.substring(16,24) + '</span>';
+      out += '<div class="text-margin'+a+'">' + mess[i].body + '<br />'+date+'</div>';
     }
     out += '</table>';
   }
   document.getElementById('messages').innerHTML = out;
+  document.getElementById("messages").setAttribute("style", "padding-top: 2.5vw;");
 }
