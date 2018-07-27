f6ab298b81ba5a114ed2ca0caf155562339052c1
Ryan <rtsfred3@gmail.com>
2018-07-27 14:11:17 -0700

    Updated /page/

---
+++ page/sw.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'GithubStatus';
+var urlsToCache = [
+  './',
+  './styling/github.css',
+  './styling/table.css',
+  './js/app.js',
+  './manifest.json',
+
+  '../img/128x128.png',
+  '../img/144x144.png',
+  '../img/refresh.svg',
+];
+
+let deferredPrompt;
+
+self.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if(choiceResult.outcome === 'accepted'){
+      console.log('User accepted the A2HS prompt');
+    }else{
+      console.log('User dismissed the A2HS prompt');
+    }
+    deferredPrompt = null;
+  });
+});
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache){
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
