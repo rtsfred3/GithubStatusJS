6b692e9ce3e57a4749a7dab90b9ee262181013b4
Ryan <rtsfred3@gmail.com>
2019-03-16 03:47:48 -0700

    Tweaks to preload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var metaColors = {'none':'#339966', 'minor':'#F1C40F', 'major':'#FF9900', 'critical':'#990000', 'unavailable':'#4F93BD', 'error':'#646464'}
-var indicatorVals = {'none':'good', 'minor':'minor', 'major':'major', 'critical':'critical', 'error':'error'}
+var indicatorVals = {'resolved':'good','none':'good', 'minor':'minor', 'major':'major', 'critical':'critical', 'error':'error'}
 var indicatorMessages = {'resolved':'good', 'investigating':'minor'}
 
 function setUp(){
@@ -51,7 +51,8 @@ function Status(arr){
 }
 
 function Messages(mess){
-    var patt = /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\/([a-zA-Z0-9-\/_.])*[^.]/i
+    var patt = /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\/([a-zA-Z0-9-\/_.])*[^.]/i;
+    var options = { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
 
     if(mess["incidents"].length == 0){
         document.getElementById('messages').innerHTML = '<div class="empty padding-none"><div class="font-36 margin-bottom">All good.</div><div class="font-12">Nothing to see here folks. Looks like GitHub is up and running and has been stable for quite some time.<br /><br />Now get back to work!</div></div>';
@@ -65,15 +66,21 @@ function Messages(mess){
             if(mess["incidents"][i]["incident_updates"].length > 0){
                 for(var j = 0; j < mess["incidents"][i]["incident_updates"].length; j++){
                     var w = (mess["incidents"][i]["incident_updates"][j]["status"] == "resolved" ? "good" : (mess["incidents"][i]["impact"] == 'none' ? 'good' : mess["incidents"][i]["impact"]));
-                    w = indicatorMessages[mess["incidents"][i]["incident_updates"][j]["status"]];
+                    // w = indicatorMessages[mess["incidents"][i]["incident_updates"][j]["status"]];
                     out += '<div class="status-box ' + w + '"><span class="message-status"><div class="right">' + w + '</div></span></div>';
 
-                    var options = { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
                     var date = new Date(mess["incidents"][i]["incident_updates"][j].created_at).toLocaleDateString("en-US", options);
 
                     date = '<span class="date empty">'+date+'</span>';
                     out += '<div class="text-margin">' + mess["incidents"][i]["incident_updates"][j].body + '<br />'+date+'</div>';
                 }
+
+                /*out += '<div class="status-box ' + mess["incidents"][i]["impact"] + '"><span class="message-status"><div class="right">' + indicatorVals[mess["incidents"][i]["impact"]] + '</div></span></div>';
+
+                var date = new Date(mess["incidents"][i].created_at).toLocaleDateString("en-US", options);
+
+                date = '<span class="date empty">'+date+'</span>';
+                out += '<div class="text-margin">' + mess["incidents"][i].name + '<br />'+date+'</div>';*/
             }
         }
         document.getElementById('messages').innerHTML = out;
