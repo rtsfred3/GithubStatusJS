41cb30bd44bb3a33799e5eb5ba2137372218c485
Ryan Fredrickson <rtsfred3@gmail.com>
2021-02-26 20:21:06 -1000

    General Quality of Life Improvements

--- icons.php
+++ icons.php
@@ -9,50 +9,109 @@
         $status = $_GET['status'];
     }
 
-    $image_width = 1200;
-    $image_height = 660;
+    function image($status){
+        $image_width  = 1200;
+        $image_height = 630;
 
-    $im = @imagecreate($image_width, $image_height) or die("Cannot Initialize new GD image stream");
+        $im = @imagecreate($image_width, $image_height) or die("Cannot Initialize new GD image stream");
 
-    // echo $json_data->status->indicator;
+        // echo $json_data->status->indicator;
 
-    if($status == "none") {
-        $background_color = imagecolorallocate($im, 51, 153, 102);
-    }elseif ($status == "minor") {
-        $background_color = imagecolorallocate($im, 219, 171, 9);
-    }elseif ($status == "major") {
-        $background_color = imagecolorallocate($im, 226, 93, 16);
-    }elseif ($status == "critical") {
-        $background_color = imagecolorallocate($im, 220, 53, 69);
-    }else {
-        $background_color = imagecolorallocate($im, 79, 147, 189);
+        if($status == "none") {
+            $background_color = imagecolorallocate($im, 51, 153, 102);
+        }elseif ($status == "minor") {
+            $background_color = imagecolorallocate($im, 219, 171, 9);
+        }elseif ($status == "major") {
+            $background_color = imagecolorallocate($im, 226, 93, 16);
+        }elseif ($status == "critical") {
+            $background_color = imagecolorallocate($im, 220, 53, 69);
+        }else {
+            $background_color = imagecolorallocate($im, 79, 147, 189);
+        }
+
+        $text_color = imagecolorallocate($im, 255, 250, 250);
+
+        $status = "GitHub Status";
+
+        // if($status == "none") { $status = "good"; }
+
+        putenv('GDFONTPATH=' . realpath('.'));
+
+        $font_size = 95;
+        $angle = 0;
+        $font = 'Verdana Bold';
+        $text = $status;
+        $name = $text . ".png";
+
+        $text_box = imagettfbbox($font_size, $angle, $font, $text);
+
+        $text_width = $text_box[2]-$text_box[0];
+        $text_height = $text_box[7]-$text_box[1];
+
+        $x = ($image_width/2) - ($text_width/2) - 5;
+        $y = ($image_height/2) - ($text_height/2);
+
+        imagettftext($im, $font_size, $angle, $x, $y, $text_color, $font, $text);
+
+        // imagepng($im);
+        imagepng($im, NULL, -1);
+        imagedestroy($im);
     }
 
-    $text_color = imagecolorallocate($im, 255, 250, 250);
+    function image_min($status){
+        $image_width  = 300;
+        $image_height = 300;
 
-    if($status == "none") { $status = "good"; }
+        $im = @imagecreate($image_width, $image_height) or die("Cannot Initialize new GD image stream");
 
-    putenv('GDFONTPATH=' . realpath('.'));
+        // echo $json_data->status->indicator;
 
-    $font_size = 95;
-    $angle = 0;
-    $font = 'Verdana Bold';
-    $text = strtoupper($status);
-    $name = $text . ".png";
+        if($status == "none") {
+            $background_color = imagecolorallocate($im, 51, 153, 102);
+        }elseif ($status == "minor") {
+            $background_color = imagecolorallocate($im, 219, 171, 9);
+        }elseif ($status == "major") {
+            $background_color = imagecolorallocate($im, 226, 93, 16);
+        }elseif ($status == "critical") {
+            $background_color = imagecolorallocate($im, 220, 53, 69);
+        }else {
+            $background_color = imagecolorallocate($im, 79, 147, 189);
+        }
 
-    $text_box = imagettfbbox($font_size, $angle, $font, $text);
+        $text_color = imagecolorallocate($im, 255, 250, 250);
 
-    $text_width = $text_box[2]-$text_box[0];
-    $text_height = $text_box[7]-$text_box[1];
+        $status = "GH";
 
-    $x = ($image_width/2) - ($text_width/2);
-    $y = ($image_height/2) - ($text_height/2);
+        // if($status == "none") { $status = "good"; }
 
-    imagettftext($im, $font_size, $angle, $x, $y, $text_color, $font, $text);
+        putenv('GDFONTPATH=' . realpath('.'));
 
-    // imagepng($im);
-    imagepng($im, NULL, -1);
-    imagedestroy($im);
+        $font_size = 95;
+        $angle = 0;
+        $font = 'Verdana Bold';
+        $text = $status;
+        $name = $text . ".png";
+
+        $text_box = imagettfbbox($font_size, $angle, $font, $text);
+
+        $text_width = $text_box[2]-$text_box[0];
+        $text_height = $text_box[7]-$text_box[1];
+
+        $x = ($image_width/2) - ($text_width/2) + 2;
+        $y = ($image_height/2) - ($text_height/2);
+
+        imagettftext($im, $font_size, $angle, $x, $y, $text_color, $font, $text);
+
+        // imagepng($im);
+        imagepng($im, NULL, -1);
+        imagedestroy($im);
+    }
+
+    if(isset($_GET['min']) || (count($argv) >= 2 && ($argv[1] == "min" || $argv[1] == "--min"))) {
+        image_min($status);
+    }else{
+        image($status);
+    }
 
     ob_flush();
 	flush();
