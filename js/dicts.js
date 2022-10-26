9f4ade94fe46a1211c2a11cda6646e50d9ca1600
Ryan Fredrickson <rtsfred3@gmail.com>
2022-10-25 23:37:02 -0700

    Creating Index & Components into SPA (#219)
    * Rewriting to SPA Web App
    
    * Adds spa.ghstatus.pages.dev to rules in JS
    
    * Fixes
    
    * More work in JS & CSS
    
    * Fixing relation assets
    
    * More fixes
    
    * Temp removal of ServiceWorker
    
    * Removing shadow off Status Window for Components
    
    * Added feature to remove invalid/irrelevant components
    
    * Using minified versions
    
    * Testing
    
    * Changing `curr` to `comp["components"][i]` to prevent 'Visit' in Comp
    
    * Making `'sw.js'` -> `'/sw.js'`
    
    * Changed PSA & Removed localhost from Router
    
    * Only shows last 7 days & Update to `sitemap.xml` & `robots.txt`
    
    * More minification
    
    * Commented out `sw.js`
    
    * Uncommented service worker & added preload for CSS & JS

---
+++ js/dicts.js
@@ -0,0 +1,79 @@
+var metaColors = {};
+var indicatorVals = {};
+var indicatorMessages = {};
+
+Object.defineProperties(metaColors, {
+    'none': { value: '#339966', enumerable: true },
+    'minor': { value: '#DBAB09', enumerable: true },
+    'major': { value: '#E25D10', enumerable: true },
+    'critical': { value: '#DC3545', enumerable: true },
+    'unavailable': { value: '#4F93BD', enumerable: true },
+    'error': { value: '#646464', enumerable: true },
+    'maintenance': { value: '#0366d6', enumerable: true },
+    
+    'under_maintenance': {
+        enumerable: true,
+        get: function(){
+            return this.maintenance;
+        }
+    }
+});
+
+Object.defineProperties(indicatorVals, {
+    'good': { value: 'good', enumerable: true },
+    'minor': { value: 'minor', enumerable: true },
+    'major': { value: 'major', enumerable: true },
+    'critical': { value: 'critical', enumerable: true },
+    'error': { value: 'error', enumerable: true },
+    'maintenance': { value: 'maintenance', enumerable: true },
+    
+    'resolved': {
+        enumerable: true,
+        get: function(){
+            return this.good;
+        }
+    },
+    'none': {
+        enumerable: true,
+        get: function(){
+            return this.good;
+        }
+    },
+    'operational': {
+        enumerable: true,
+        get: function(){
+            return this.good;
+        }
+    },
+    'degraded_performance': {
+        enumerable: true,
+        get: function(){
+            return this.minor;
+        }
+    },
+    'partial_outage': {
+        enumerable: true,
+        get: function(){
+            return this.major;
+        }
+    },
+    'major_outage': {
+        enumerable: true,
+        get: function(){
+            return this.critical;
+        }
+    },
+    'under_maintenance': {
+        enumerable: true,
+        get: function(){
+            return this.maintenance;
+        }
+    }
+});
+
+Object.defineProperties(indicatorMessages, {
+    'resolved': { value: 'good', enumerable: true },
+    'investigating': { value: 'minor', enumerable: true },
+    'critical': { value: 'critical', enumerable: true },
+    'maintenance': { value: 'maintenance', enumerable: true }
+});
