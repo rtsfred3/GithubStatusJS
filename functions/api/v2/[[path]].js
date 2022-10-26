de3584349a498eb6d8a5fec0da20280e879f9552
Ryan Fredrickson <rtsfred3@gmail.com>
2022-10-26 03:15:55 -0700

    Another tweak to [[path]].js

--- functions/api/v2/[[path]].js
+++ functions/api/v2/[[path]].js
@@ -1,13 +1,13 @@
 const validPath = ['summary.json', 'status.json', 'components.json', 'incidents/unresolved.json', 'incidents.json', 'scheduled-maintenances/upcoming.json', 'scheduled-maintenances/active.json', 'scheduled-maintenances.json'];
 
 export async function onRequestGet({ params }) {
-    if(!validPath.includes(params.path)){
-        return new Response(JSON.stringify({'path': params.path}), {
+    if(!validPath.includes(params.path[0])){
+        return new Response(JSON.stringify({ 'path': params.path[0] }), {
             headers: { "Content-Type": "application/json" },
         });
     }
     
-    const res = await fetch(`https://www.githubstatus.com/api/v2/${params.path}`);
+    const res = await fetch(`https://www.githubstatus.com/api/v2/${params.path[0]}`);
     
     const data = await res.json();
     
