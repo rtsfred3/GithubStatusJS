01090ed6f3fe233c5186fb23d8b69661b6065500
Ryan Fredrickson <rtsfred3@gmail.com>
2022-10-26 03:20:37 -0700

    Fixing `pathname` in `const res`

--- functions/api/v2/[[path]].js
+++ functions/api/v2/[[path]].js
@@ -2,17 +2,17 @@ const validPath = ['summary.json', 'status.json', 'components.json', 'incidents/
 
 export async function onRequestGet({ params }) {
     var pathname = params.path.join('/');
-    // if(!validPath.includes(pathname)){
+    if(!validPath.includes(pathname)){
         return new Response(JSON.stringify({ 'path': pathname }), {
             headers: { "Content-Type": "application/json" },
         });
-    // }
-    //
-    // const res = await fetch(`https://www.githubstatus.com/api/v2/${params.path[0]}`);
-    //
-    // const data = await res.json();
-    //
-    // const info = JSON.stringify(data, null, 2);
-    //
-    // return new Response(info);
+    }
+    
+    const res = await fetch(`https://www.githubstatus.com/api/v2/${pathname}`);
+    
+    const data = await res.json();
+    
+    const info = JSON.stringify(data, null, 2);
+    
+    return new Response(info);
 }
