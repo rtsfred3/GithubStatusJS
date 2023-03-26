4fff9d90c23d56668b67dd555c952b6a6f066359
Ryan Fredrickson <rtsfred3@gmail.com>
2023-03-26 00:06:37 -0700

    Update

--- apps/status/sw.js
+++ apps/status/sw.js
@@ -1,6 +1,14 @@
 var OFFLINE_CACHE = 'min_offline';
 
-var offlineFile = 'error.html';
+var offlineFile = 'index.html';
+
+function PeriodSyncUpdateFiles(){
+    const periodicSync = self.registration.periodicSync;
+
+    periodicSync.register('update-files', { minInterval: 24 * 60 * 60 * 1000 }).catch(function(err) {
+        console.log(err);
+    });
+}
 
 self.addEventListener('install', function(event) {
     self.skipWaiting();
@@ -12,6 +20,8 @@ self.addEventListener('install', function(event) {
                 return cache.put(offlinePage, response);
             });
         }));
+        
+    PeriodSyncUpdateFiles();
 });
 
 self.addEventListener('fetch', function(event) {
@@ -27,4 +37,36 @@ self.addEventListener('refreshOffline', function(response) {
     return caches.open(OFFLINE_CACHE).then(function(cache) {
         return cache.put(offlinePage, response);
     });
+});
+
+self.addEventListener('sync', (event) => {
+    if (event.tag === 'update-files') {
+        console.log(event.tag);
+        
+        var offlinePage = new Request(offlineFile);
+        
+        event.waitUntil(
+            fetch(offlinePage).then(function(response) {
+                return caches.open(OFFLINE_CACHE).then(function(cache) {
+                    return cache.put(offlinePage, response);
+                });
+            })
+        );
+    }
+});
+
+self.addEventListener('periodicSync', (event) => {
+    if (event.tag === 'update-files') {
+        console.log(event.tag);
+        
+        var offlinePage = new Request(offlineFile);
+        
+        event.waitUntil(
+            fetch(offlinePage).then(function(response) {
+                return caches.open(OFFLINE_CACHE).then(function(cache) {
+                    return cache.put(offlinePage, response);
+                });
+            })
+        );
+    }
 });
