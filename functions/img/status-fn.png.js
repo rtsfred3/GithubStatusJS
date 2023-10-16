c90e0052469282ce83157ce1571cee5f06b87888
Ryan Fredrickson <rtsfred3@gmail.com>
2023-10-15 21:46:00 -0700

    creating function for status png

---
+++ functions/img/status-fn.png.js
@@ -0,0 +1,21 @@
+export async function onRequestGet({ params, env }) {
+    const status_images = env.status_images;
+
+    const statusRes = await fetch("https://www.githubstatus.com/api/v2/status.json");
+    const statusData = await statusRes.json();
+
+    var status = statusData.status.indicator;
+
+    if(status == "none"){
+        status = "good";
+    }
+
+    const img = await KV_IMG.get(status);
+    
+    return new Response(img, {
+        headers: {
+            "Content-Type": "application/png",
+            "Cache-Control": "max-age=60, public"
+        },
+    });
+}
