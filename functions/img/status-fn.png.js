c83e2175c7266b206e2a25e1605a8a36b6a6a554
Ryan Fredrickson <rtsfred3@gmail.com>
2023-10-15 22:00:03 -0700

    Update status-fn.png.js

--- functions/img/status-fn.png.js
+++ functions/img/status-fn.png.js
@@ -1,3 +1,13 @@
+function base64Decode (string) {
+    string = atob(string);
+    const
+      length = string.length,
+      buf = new ArrayBuffer(length),
+      bufView = new Uint8Array(buf);
+    for (var i = 0; i < length; i++) { bufView[i] = string.charCodeAt(i) }
+    return buf
+}
+
 export async function onRequestGet({ params, env }) {
     const status_images = env.status_images;
 
@@ -10,9 +20,10 @@ export async function onRequestGet({ params, env }) {
         status = "good";
     }
 
-    const imgRes = await fetch(`https://githubstat.us/img/status/status-${status}.png`);
+    var statusImage = await env.status_images.get(`${status}`);
+    statusImage = statusImage.replace("data:image/png;base64,", "");
     
-    return new Response(imgRes, {
+    return new Response(base64Decode(statusImage), {
         headers: {
             "Content-Type": "application/png",
             "Cache-Control": "max-age=60, public"
