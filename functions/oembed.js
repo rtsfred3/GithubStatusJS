69aa1fecc17f78035d017f92f1d59fcb61f58b5d
Ryan Fredrickson <rtsfred3@gmail.com>
2023-10-21 19:46:24 -0700

    Testing oembed

---
+++ functions/oembed.js
@@ -0,0 +1,20 @@
+export async function onRequestGet({ params, env }) {
+    const db = env.CACHE_DB;
+    const table = env.TABLE;
+
+    const { results } = await db.prepare(`SELECT * FROM ${table} WHERE route = ? OR route = ?`).bind(`/api/v2/status.json`).first();
+
+    var result = results;
+
+    console.log(result);
+
+    var info = JSON.stringify(result, null, 2);
+    
+    return new Response(info, {
+        headers: {
+            "Content-Type": "application/json",
+            "access-control-allow-origin": "*",
+            "Cache-Control": "max-age=3600, public"
+        },
+    });
+}
