e21026c876fc91783cba4184a970026b186815b4
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-08 04:38:10 -0700

    Update status.js

--- functions/status.js
+++ functions/status.js
@@ -38,7 +38,7 @@ export async function onRequestGet({ request, params, env }) {
     var canonicalUrlList = [...AmpHtml.matchAll(canonicalUrlRegex)];
     var canonicalUrl = new URL(`https://${canonicalUrlList[0][1]}`);
 
-    var statusJson = JSON.parse(results[0].data);
+    // var statusJson = JSON.parse(results[0].data);
 
     // var StatuspageStatus = CapitalizeFirstLetter(statusJson.status.indicator == "none" ? "good" : statusJson.status.indicator);
     // var StatuspageDescription = statusJson.status.description;
@@ -64,11 +64,11 @@ export async function onRequestGet({ request, params, env }) {
 
     html = html.replaceAll(`${newBaseUrl.host}${canonicalUrl.pathname}`, `${newBaseUrl.host}${newBaseUrl.pathname}`);
 
-    for (const img of DeduplicateArrayOfArrays([...AmpHtml.matchAll(imageUrlRegex)])) {
+    /* for (const img of DeduplicateArrayOfArrays([...AmpHtml.matchAll(imageUrlRegex)])) {
         console.log(img);
 
         html = html.replaceAll(img[0], img[0].replace('good', StatuspageStatus.toLowerCase()));
-    }
+    } */
 
     for(const title of DeduplicateArrayOfArrays([...AmpHtml.matchAll(titleRegex)])){
         console.log(title);
@@ -81,15 +81,15 @@ export async function onRequestGet({ request, params, env }) {
         // }
     }
 
-    for(const description of DeduplicateArrayOfArrays([...new Set(AmpHtml.matchAll(descriptionRegex))])){
+    /* for(const description of DeduplicateArrayOfArrays([...new Set(AmpHtml.matchAll(descriptionRegex))])){
         console.log(description);
 
-        // if (description[1] == StatuspageName || isStatuspageNameSame) {
-        //     html = html.replaceAll(description[0], `${description[0]} | ${StatuspageDescription}`);
-        // } else {
-        //     html = html.replaceAll(description[0], `${description[0].replace(description[1], StatuspageName)} | ${StatuspageDescription}`);
-        // }
-    }
+        if (description[1] == StatuspageName || isStatuspageNameSame) {
+            html = html.replaceAll(description[0], `${description[0]} | ${StatuspageDescription}`);
+        } else {
+            html = html.replaceAll(description[0], `${description[0].replace(description[1], StatuspageName)} | ${StatuspageDescription}`);
+        }
+    } */
 
     for(const url of [...new Set(AmpHtml.match(/"\/\/([a-z]|\.)+\//g))].map((u) => u.substring(1))){
         html = html.replaceAll(url, `//${StatuspageUrl}/`);
