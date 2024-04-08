e1e220dba45a96ba4c73ae193d51fae86309173c
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-08 00:49:22 -0700

    Update amp_test.js

--- functions/amp_test.js
+++ functions/amp_test.js
@@ -5,11 +5,11 @@ import StatuspageAmpHtml from "../StatuspageHTML/amp/index.html";
 export async function onRequestGet({ request, params, env }) {
     console.log(request.url);
 
-    var html = StatuspageAmpHtml.replace('spstat.us', 'githubstat.us');
+    // var html = StatuspageAmpHtml.replace('spstat.us', 'githubstat.us');
 
-    return new Response(html, {
+    return new Response(AmpHtml, {
         headers: {
-            "Content-Type": "application/html",
+            "Content-Type": "text/html; charset=utf-8",
             "access-control-allow-origin": "*",
             "Cache-Control": "max-age=3600, public"
         },
