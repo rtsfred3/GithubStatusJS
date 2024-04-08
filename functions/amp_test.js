89213cb20d20e8d6ac9fc6f6496865b4d9977af2
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-08 02:24:05 -0700

    Updating description

--- functions/amp_test.js
+++ functions/amp_test.js
@@ -8,10 +8,13 @@ function capitalizeFirstLetter(string) {
 
 export async function onRequestGet({ request, params, env }) {
     var StatuspageUrl = 'www.githubstatus.com';
-    var newBaseUrl = new URL(request.url);//.host;
+    var newBaseUrl = new URL(request.url);
     var oldBaseUrl = "githubstat.us";
     var titleRegex = /([A-Za-z]*) Status - AMP/g;
     var descriptionRegex = /A minified AMP website to monitor [A-Za-z]* status updates./g;
+    var canonicalUrlRegex = /<link rel="canonical" href="https:\/\/(([a-z]|\.)+\/([a-z]|\/)+)" \/>/g;
+
+    console.log([...AmpHtml.matchAll(canonicalUrlRegex)]);
 
     const statusRes = await fetch(`https://${StatuspageUrl}/api/v2/status.json`);
     const statusData = await statusRes.json();
@@ -20,14 +23,14 @@ export async function onRequestGet({ request, params, env }) {
     var StatuspageDescription = statusData.status.description;
     var StatuspageName = statusData.page.name;
 
-    var html = AmpHtml.replaceAll(oldBaseUrl, newBaseUrl);
+    var html = AmpHtml.replaceAll(oldBaseUrl, newBaseUrl.host);
 
     for(const title of [...new Set(AmpHtml.matchAll(titleRegex))]){
         html = html.replaceAll(title[0], `${title[0].replace(title[1], StatuspageName)} | ${StatuspageStatus}`);
     }
 
-    for(const description of [...new Set(AmpHtml.match(descriptionRegex))]){
-        html = html.replaceAll(description, `${description} | ${StatuspageDescription}`);
+    for(const description of [...new Set(AmpHtml.matchAll(descriptionRegex))]){
+        html = html.replaceAll(description[0], `${description[0].replace(description[1], StatuspageName)} | ${StatuspageDescription}`);
     }
 
     for(const url of [...new Set(AmpHtml.match(/"\/\/([a-z]|\.)+\//g))].map((u) => u.substring(1))){
