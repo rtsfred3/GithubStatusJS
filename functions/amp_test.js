98216c6f31ffb588e7de35ad612ca16b0bdca9b8
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-08 01:50:50 -0700

    Update amp_test.js

--- functions/amp_test.js
+++ functions/amp_test.js
@@ -32,11 +32,12 @@ export async function onRequestGet({ request, params, env }) {
         html = html.replaceAll(description, `${description} | ${StatuspageDescription}`);
     }
 
-    var StatuspageUrls = [...new Set(AmpHtml.match(/\/\/([a-z]|\.)+\//g))];
-    StatuspageUrls = StatuspageUrls.filter(u => u != oldBaseUrl);
-    // var currentStatuspageUrl = StatuspageUrls[0].splice(1);
+    var StatuspageUrls = [...new Set(AmpHtml.match(/"\/\/([a-z]|\.)+\//g))];
+    // StatuspageUrls = StatuspageUrls.filter(u => u != `//${oldBaseUrl}/`);
+    var currentStatuspageUrl = StatuspageUrls[0].splice(1);
 
     console.log(StatuspageUrls);
+    console.log(currentStatuspageUrl);
 
     return new Response(html, {
         headers: {
