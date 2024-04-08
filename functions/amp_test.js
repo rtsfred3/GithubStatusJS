c322449185e0cc8369337b7c009814d65fb35cf5
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-08 02:09:59 -0700

    Update amp_test.js

--- functions/amp_test.js
+++ functions/amp_test.js
@@ -10,7 +10,7 @@ export async function onRequestGet({ request, params, env }) {
     var StatuspageUrl = 'www.githubstatus.com';
     var newBaseUrl = new URL(request.url);//.host;
     var oldBaseUrl = "githubstat.us";
-    var titleRegex = /[A-Za-z]* Status - AMP/g;
+    var titleRegex = /([A-Za-z]*) Status - AMP/g;
     var descriptionRegex = /A minified AMP website to monitor [A-Za-z]* status updates./g;
 
     const statusRes = await fetch(`https://${StatuspageUrl}/api/v2/status.json`);
@@ -20,6 +20,8 @@ export async function onRequestGet({ request, params, env }) {
     var StatuspageDescription = statusData.status.description;
 
     var html = AmpHtml.replaceAll(oldBaseUrl, newBaseUrl);
+
+    console.log(AmpHtml.matchAll(titleRegex));
 
     for(const title of [...new Set(AmpHtml.match(titleRegex))]){
         html = html.replaceAll(title, `${title} | ${StatuspageStatus}`);
