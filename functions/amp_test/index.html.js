3f780d909e93714d6d8139febd1783f1a59e750e
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-08 01:13:19 -0700

    Update index.html.js

--- functions/amp_test/index.html.js
+++ functions/amp_test/index.html.js
@@ -1,17 +1,23 @@
-import AmpHtml from "../amp/index.html";
+import AmpHtml from "../../amp/index.html";
 
-import StatuspageAmpHtml from "../StatuspageHTML/amp/index.html";
+// import StatuspageAmpHtml from "../StatuspageHTML/amp/index.html";
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export async function onRequestGet({ request, params, env }) {
+    var StatuspageUrl = 'www.githubstatus.com';
     var newBaseUrl = new URL(request.url);
     var oldBaseUrl = "githubstat.us";
-    var title = "GitHub Status - AMP";
+    var title = 'GitHub Status - AMP';
     var description = 'A minified AMP website to monitor GitHub status updates.';
 
-    const statusRes = await fetch("https://www.githubstatus.com/api/v2/status.json");
+    const statusRes = await fetch(`https://${StatuspageUrl}/api/v2/status.json`);
     const statusData = await statusRes.json();
 
-    var status = statusData.status.indicator == "none" ? "Good" : statusData.status.indicator;
+    var status = statusData.status.indicator == "none" ? "good" : statusData.status.indicator;
+    status = capitalizeFirstLetter(status);
 
     var html = AmpHtml.replace(oldBaseUrl, newBaseUrl);
     html = html.replace(title, `${title} | ${statusData.status.description}`);
