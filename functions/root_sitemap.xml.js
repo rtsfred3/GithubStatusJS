4cd803c1e000341b5416453f594b046687259190
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-08 03:53:34 -0700

    Update root_sitemap.xml.js

--- functions/root_sitemap.xml.js
+++ functions/root_sitemap.xml.js
@@ -2,31 +2,27 @@ export async function onRequestGet({ request, params, env }) {
     var currDate = new Date();
     currDate.setHours(currDate.getHours(), 0, 0, 0);
 
+    var newBaseUrl = new URL(request.url);
+
     var date = currDate.toJSON();
 
     var urls = [
-        "https://githubstat.us/",
-        "https://githubstat.us/components/",
-        "https://githubstat.us/status/",
-        "https://githubstat.us/amp/",
-        "https://githubstat.us/amp_test/",
-        "https://githubstat.us/StatuspageHTML/",
-        "https://githubstat.us/Statuspage/",
-        "https://githubstat.us/apps/app/",
-        "https://githubstat.us/apps/components/",
-        "https://githubstat.us/apps/lang/",
-        "https://githubstat.us/apps/min/",
-        "https://githubstat.us/apps/status/",
-        "https://githubstat.us/apps/status/french/",
+        "/",
+        "/components/",
+        "/status/",
+        "/amp/",
+        "/amp_test/",
+        "/StatuspageHTML/",
+        "/Statuspage/",
+        "/apps/app/",
+        "/apps/components/",
+        "/apps/lang/",
+        "/apps/min/",
+        "/apps/status/",
+        "/apps/status/french/",
     ]
 
-    // var newXml = '';
-    
-    // for (const url of urls) {
-    //     newXml += `<url><loc>${url}</loc><lastmod>${date}</lastmod><changefreq>hourly</changefreq></url>`;
-    // }
-
-    var xmlElements = urls.map((url) => `<url><loc>${url}</loc><lastmod>${date}</lastmod><changefreq>hourly</changefreq></url>`);
+    var xmlElements = urls.map((url) => `<url><loc>${newBaseUrl.protocol}${newBaseUrl.host}${url}</loc><lastmod>${date}</lastmod><changefreq>hourly</changefreq></url>`);
 
     return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> \
         ${xmlElements.join("")} \
