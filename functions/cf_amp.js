ef154e256a1e659b0b653aa4d699e091a6e25763
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-08 21:56:33 -0700

    Update cf_amp.js

--- functions/cf_amp.js
+++ functions/cf_amp.js
@@ -2,8 +2,5 @@ import Path from './lib/Path.js';
 import ModifyHTML from './lib/ModifyHTML.js';
 
 export async function onRequestGet({ request, params, env }) {
-    console.log(StatuspageDictionary.IndicatorVals);
-    
-    var response = await ModifyHTML(request, env, Path.Amp);
-    return response;
+    return await ModifyHTML(request, env, Path.Amp);
 }
