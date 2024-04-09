55b54af14945a063cd5c3af237ab002750cdd211
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-09 02:22:54 -0700

    Testing

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+import Path from './lib/Path.js';
+import ModifyHTML from './lib/ModifyHTML.js';
+
+import Html from "./partial_html/amp_template.html";
+
+export async function onRequestGet(context) {
+    const url = new URL(context.request.url);
+    const cacheKey = new Request(url.toString(), context.request);
+    const cache = caches.default;
+    let response = await cache.match(cacheKey);
+
+    if (response) {
+        return response;
+    }
+    
+    response = new Response(Html, {
+        headers: {
+            "Content-Type": "text/html; charset=utf-8",
+            "access-control-allow-origin": "*",
+            "X__X": "TEST",
+            "Cache-Control": `max-age=${context.env.CACHE_AGE}, s-maxage=${context.env.CACHE_AGE}, public`,
+            "Cloudflare-CDN-Cache-Control": `max-age=${context.env.CACHE_AGE}`
+        },
+    });
+
+    context.waitUntil(cache.put(cacheKey, response.clone()));
+
+    return response;
+}
