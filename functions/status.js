bae340eee1e82ab2bd66acc6db8f918106a55601
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-09 19:36:41 -0700

    Update status.js

--- functions/status.js
+++ functions/status.js
@@ -6,7 +6,7 @@ import Html from "../n_index.html";
 import UserAgents from './lib/UserAgents.js';
 
 export async function onRequestGet(context) {
-    isBot = UserAgents.IsBot(context.request.headers.get('user-agent'));
+    var isBot = UserAgents.IsBot(context.request.headers.get('user-agent'));
 
     if (isBot) {
         return await ModifyHTML(context, Path.Status);
