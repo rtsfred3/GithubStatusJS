5a253c55b63a39e8d2b3ddb7864ce67768754c37
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-09 21:50:26 -0700

    Fixed `TimeSpans`

--- functions/lib/TimeSpans.js
+++ functions/lib/TimeSpans.js
@@ -1,6 +1,6 @@
 export default class TimeSpans {
     static get Hour() { return 60*60; }
-    static get Day() { return TimeSpans.Day * 24; }
+    static get Day() { return TimeSpans.Hour * 24; }
     static get Week() { return TimeSpans.Day * 7; }
     static get Month() { return TimeSpans.Week * 4; }
     static get Year() { return TimeSpans.Day * 365; }
