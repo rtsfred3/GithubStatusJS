6d42b8620103b2f4db01597d4ce96913439c5709
Ryan Fredrickson <rtsfred3@gmail.com>
2024-04-29 19:54:36 -0700

    Testing

--- modules/Statuspage.esm.js
+++ modules/Statuspage.esm.js
@@ -164,6 +164,7 @@ class StatuspageDictionary {
             Status: 'Status',
             Component: 'Component',
             Amp: 'Amp',
+            Maintenance: 'Maintenance',
             Error: 'Error'
         });
     }
@@ -174,6 +175,7 @@ class StatuspageDictionary {
             [this.PathNames.Status]: `(Unofficial) Mini {{SiteName}} Status`,
             [this.PathNames.Component]: `(Unofficial) {{SiteName}} Status Components`,
             [this.PathNames.Amp]: `(Unofficial) {{SiteName}} Status AMP`,
+            [this.PathNames.Maintenance]: `Page Under Maintenance`,
             [this.PathNames.Error]: `(Unofficial) {{SiteName}} Status - Error`,
         };
 
