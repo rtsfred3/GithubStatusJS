cf683601d3a867fe6fbd38db0f290263e3aa4967
Ryan Fredrickson <rtsfred3@gmail.com>
2024-05-11 12:48:07 -0700

    Updating manifest.json

--- scripts/regex_html.sh
+++ scripts/regex_html.sh
@@ -1,21 +1,18 @@
 echo "Version $1";
 
-if [ ! -d StatuspageHTML/tmp/ ]; then
-    mkdir StatuspageHTML/tmp/
-    mkdir StatuspageHTML/tmp/amp/
-    mkdir StatuspageHTML/tmp/preact/
-    mkdir StatuspageHTML/tmp/static/
-fi;
+if [ ! -d StatuspageHTML/tmp/ ]; then mkdir StatuspageHTML/tmp/; fi;
+if [ ! -d StatuspageHTML/tmp/amp/ ]; then mkdir StatuspageHTML/tmp/amp/; fi;
+if [ ! -d StatuspageHTML/tmp/preact/ ]; then mkdir StatuspageHTML/tmp/preact/; fi;
+if [ ! -d StatuspageHTML/tmp/static/ ]; then mkdir StatuspageHTML/tmp/static/; fi;
 
-if [ ! -d StatuspageHTML/output/ ]; then
-    mkdir StatuspageHTML/output/
-    mkdir StatuspageHTML/output/amp/
-    mkdir StatuspageHTML/output/preact/
-    mkdir StatuspageHTML/output/static/
-fi;
+if [ ! -d StatuspageHTML/output/ ]; then mkdir StatuspageHTML/output/; fi;
+if [ ! -d StatuspageHTML/output/amp/ ]; then mkdir StatuspageHTML/output/amp/; fi;
+if [ ! -d StatuspageHTML/output/preact/ ]; then mkdir StatuspageHTML/output/preact/; fi;
+if [ ! -d StatuspageHTML/output/static/ ]; then mkdir StatuspageHTML/output/static/; fi;
 
 sed -re 's/( )*<!--([[:space:]]|[a-zA-Z0-9]|[=\.\/"<>:-])+-->$//g' -e "s/{{version}}/${1}/g" -e 's/v([0-9]{1,2})-([0-9]{1,2})-.([0-9]{1,2})/v([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,2})/g' StatuspageHTML/index.html > StatuspageHTML/tmp/index.html
 
+sed -u -r -e 's/( )*<!--([[:space:]]|[a-zA-Z0-9]|[=\.\/"<>:-])+-->$//g' -e 's/-good\.(png|webp)/-unavailable\.\1/g' -e 's/{{status}}/loading/g' -e 's/{{ThemeColor}}/#4F93BD/g' -e 's/{{path}}//g' -e "s/{{version}}/${1}/g" StatuspageHTML/static/index.template.html > StatuspageHTML/tmp/static/index.shell.html
 sed -u -r -e 's/( )*<!--([[:space:]]|[a-zA-Z0-9]|[=\.\/"<>:-])+-->$//g' -e 's/-good\.(png|webp)/-unavailable\.\1/g' -e 's/{{status}}/loading/g' -e 's/{{ThemeColor}}/#4F93BD/g' -e 's/{{path}}/status\//g' -e "s/{{version}}/${1}/g" StatuspageHTML/static/index.template.html > StatuspageHTML/tmp/static/status.shell.html
 sed -u -r -e 's/( )*<!--([[:space:]]|[a-zA-Z0-9]|[=\.\/"<>:-])+-->$//g' -e 's/-good\.(png|webp)/-unavailable\.\1/g' -e 's/{{status}}/loading/g' -e 's/{{ThemeColor}}/#4F93BD/g' -e 's/{{path}}/components\//g' -e "s/{{version}}/${1}/g" StatuspageHTML/static/index.template.html > StatuspageHTML/tmp/static/components.shell.html
 sed -u -r -e 's/( )*<!--([[:space:]]|[a-zA-Z0-9]|[=\.\/"<>:-])+-->$//g' -e 's/-good\.(png|webp)/-error\.\1/g' -e 's/{{status}}/error/g' -e 's/{{ThemeColor}}/#646464/g' -e 's/{{path}}//g' -e "s/{{version}}/${1}/g" StatuspageHTML/static/index.template.html > StatuspageHTML/tmp/static/index.error.html
@@ -26,6 +23,7 @@ sed -e "s/{{version}}/${1}/g" preact/index.html > StatuspageHTML/tmp/preact/inde
 
 if [ -f StatuspageHTML/tmp/index.html ]; then cat StatuspageHTML/tmp/index.html > StatuspageHTML/output/index.html; fi;
 
+if [ -f StatuspageHTML/tmp/static/index.shell.html ]; then cat StatuspageHTML/tmp/static/index.shell.html > StatuspageHTML/output/static/index.shell.html; fi;
 if [ -f StatuspageHTML/tmp/static/status.shell.html ]; then cat StatuspageHTML/tmp/static/status.shell.html > StatuspageHTML/output/static/status.shell.html; fi;
 if [ -f StatuspageHTML/tmp/static/status.shell.html ]; then cat StatuspageHTML/tmp/static/components.shell.html > StatuspageHTML/output/static/components.shell.html; fi;
 if [ -f StatuspageHTML/tmp/static/index.error.html ]; then cat StatuspageHTML/tmp/static/index.error.html > StatuspageHTML/output/static/index.error.html; fi;
