62dcda1d97e50d507696d0fd33af1154b5918045
Ryan Fredrickson <rtsfred3@gmail.com>
2024-05-11 10:18:28 -0700

    Testing

--- scripts/regex_html.sh
+++ scripts/regex_html.sh
@@ -1,3 +1,5 @@
+echo "Version $1";
+
 sed -e 's/{{version}}/${{ steps.version.outputs.version }}/g' -re 's/( )*<!--([[:space:]]|[a-zA-Z0-9]|[=\.\/"<>:-])+-->$//g' -e 's/v([0-9]{1,2})-([0-9]{1,2})-.([0-9]{1,2})/v([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,2})/g' StatuspageHTML/index.html > StatuspageHTML/index.tmp.html
 
 sed -u -r -e 's/( )*<!--([[:space:]]|[a-zA-Z0-9]|[=\.\/"<>:-])+-->$//g' -e 's/{{status}}/loading/g' -e 's/{{ThemeColor}}/#4F93BD/g' -e 's/{{path}}/status\//g' -e 's/{{version}}/${{ steps.version.outputs.version }}/g' StatuspageHTML/static/index.template.html > StatuspageHTML/static/status.shell.tmp.html
@@ -8,12 +10,22 @@ sed -u -r -e 's/( )*<!--([[:space:]]|[a-zA-Z0-9]|[=\.\/"<>:-])+-->$//g' -e 's/{{
 sed -e 's/{{version}}/${{ steps.version.outputs.version }}/g' StatuspageHTML/amp/index.html > StatuspageHTML/amp/index.tmp.html
 sed  -e 's/{{version}}/${{ steps.version.outputs.version }}/g' preact/index.html > preact/index.tmp.html
 
-IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/index.tmp.html > StatuspageHTML/index.html
+if [ -f StatuspageHTML/index.tmp.html ]; then cat StatuspageHTML/index.tmp.html > StatuspageHTML/index.html; fi;
 
-IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/static/status.shell.tmp.html > StatuspageHTML/static/status.shell.html
-IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/static/components.shell.tmp.html > StatuspageHTML/static/components.shell.html
-IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/static/index.error.tmp.html > StatuspageHTML/static/index.error.html
-IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/static/index.maintenance.tmp.html > StatuspageHTML/static/index.maintenance.html
+if [ -f StatuspageHTML/static/status.shell.tmp.html ]; then cat StatuspageHTML/static/status.shell.tmp.html > StatuspageHTML/static/status.shell.html; fi;
+if [ -f StatuspageHTML/static/status.shell.tmp.html ]; then cat StatuspageHTML/static/components.shell.tmp.html > StatuspageHTML/static/components.shell.html; fi;
+if [ -f StatuspageHTML/static/index.error.tmp.html ]; then cat StatuspageHTML/static/index.error.tmp.html > StatuspageHTML/static/index.error.html; fi;
+if [ -f StatuspageHTML/static/index.maintenance.tmp.html ]; then cat StatuspageHTML/static/index.maintenance.tmp.html > StatuspageHTML/static/index.maintenance.html; fi;
 
-IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/amp/index.tmp.html > StatuspageHTML/amp/index.html
-IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < preact/index.tmp.html > preact/index.html
+if [ -f StatuspageHTML/amp/index.tmp.html ]; then cat StatuspageHTML/amp/index.tmp.html > StatuspageHTML/amp/index.html; fi;
+if [ -f preact/index.tmp.html ]; then cat preact/index.tmp.html > preact/index.html; fi;
+
+# IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/index.tmp.html > StatuspageHTML/index.html
+
+# IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/static/status.shell.tmp.html > StatuspageHTML/static/status.shell.html
+# IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/static/components.shell.tmp.html > StatuspageHTML/static/components.shell.html
+# IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/static/index.error.tmp.html > StatuspageHTML/static/index.error.html
+# IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/static/index.maintenance.tmp.html > StatuspageHTML/static/index.maintenance.html
+
+# IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < StatuspageHTML/amp/index.tmp.html > StatuspageHTML/amp/index.html
+# IFS=''; while read -r line; do  if [[ ${#line} -gt 0 ]]; then  echo "$line"; fi; done < preact/index.tmp.html > preact/index.html
